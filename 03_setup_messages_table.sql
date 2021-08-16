b26c85cac83e9ee401f7fe227ea7c731029357b5
Mark Burggraf <markb@mantisbible.com>
2021-08-16 06:53:57 -0700

    lock down messages table by default

--- 03_setup_messages_table.sql
+++ 03_setup_messages_table.sql
@@ -16,4 +16,11 @@ CREATE TABLE if not exists public.messages
     deliveryresult jsonb,
     deliverysignature jsonb,
     log jsonb
-)
+);
+ALTER TABLE public.messages OWNER TO supabase_admin;
+ALTER TABLE public.messages ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "messages delete policy" ON public.messages FOR DELETE USING (false);
+CREATE POLICY "messages insert policy" ON public.messages FOR INSERT WITH CHECK (false);
+CREATE POLICY "messages select policy" ON public.messages FOR SELECT USING (false);
+CREATE POLICY "messages update policy" ON public.messages FOR UPDATE USING (false) WITH CHECK (false);
+
